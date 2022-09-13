499cf8b59bd0e1540c1344266927f59c1d5cc4b4
TLNur <113358076+TLNur@users.noreply.github.com>
2022-09-13 12:34:23 +0500

    update

--- test_class.py
+++ test_class.py
@@ -1,6 +1,16 @@
-# content of test_class.py
-class TestClass:
-    def test_one(self):
-        x = "this"
-        assert "h" in x
+import pytest
 
+class TestClassList:
+
+    def test_li(self):
+        myList = [-100,-1,0,1,100]
+        for x in (myList):
+            if x % 2 == 0:
+                print ('is even')
+            else:
+                print ('is odd')
+        assert len(myList) == 5
+
+    def test_float(self):
+        myList = [-100,-1,0,1,100]
+        assert test_li(myList)
